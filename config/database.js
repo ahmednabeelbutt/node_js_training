6415c04ccb5296ee29507bd0ea363f08ad813728
SYSTEMS\ahmed.nabeel <ahmed.nabeel@systemsltd.com>
2023-07-03 15:55:25 +0500

    ass#4

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
 exports.connect = () => {
+    
+    const connectionString = process.env.DB_CONNECTION_STRING
+    .replace('$DB_USERNAME', process.env.DB_USERNAME)
+    .replace('$DB_PASSWORD', process.env.DB_PASSWORD);
 
     mongoose
-    .connect("mongodb+srv://admin123:admin123@nodetraining.4eszwi2.mongodb.net/")
+    .connect(connectionString)
     .then(() => {
         console.log("Successfully connected to db")
     })
